f26fed440892e7175e0e1b18bae8347c44f68dc3
Priyanshi Chauhan <107524071+Cpriyanshi77@users.noreply.github.com>
2022-11-14 20:15:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.7"
+    }
+  }
+}
 
+provider "aws" {
+  region  = "us-west-2"
+  access_key = "AKIAYIPI5632FTKC5KTR"
+  secret_key = "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"
+}
